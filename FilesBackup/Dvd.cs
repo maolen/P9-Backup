dfc4e2dd1848c2acf6100f0eedcd27eaa292df69
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-07-25 17:19:06 +0600

    feat: заполнен класс Dvd свойствами

--- FilesBackup/Dvd.cs
+++ FilesBackup/Dvd.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace FilesBackup
 {
-    class Dvd
+    public class Dvd : Storage
     {
+        public bool isTwoSided { get; set; }
+
+        public Dvd()
+        {
+            if(isTwoSided)
+            {
+                TotalStorage = StorageConstants.DVD_TWO_SIDE_STORAGE;
+            }
+            else
+            {
+                TotalStorage = StorageConstants.DVD_ONE_SIDE_STORAGE;
+            }
+            ReadSpeed = StorageConstants.DVD_READ_SPEED;
+            WriteSpeed = StorageConstants.DVD_WRITE_SPEED;
+        }
+
+        public override void CopyData()
+        {
+            throw new NotImplementedException();
+        }
+        public override string GetDeficeInfo()
+        {
+            var additionalInfo = new StringBuilder();
+            if(isTwoSided)
+            {
+                additionalInfo.Append("\nТип DVD: двухсторонний");
+            }
+            else
+            {
+                additionalInfo.Append("\nТип DVD: односторонний");
+            }
+            return base.GetDeficeInfo() + additionalInfo.ToString();
+        }
     }
 }
--- FilesBackup/StorageConstants.cs
+++ FilesBackup/StorageConstants.cs
@@ -6,6 +6,8 @@
         public const double USB_2_WRITE_SPEED = 8.2;
         public const double USB_3_READ_SPEED = 57.5;
         public const double USB_3_WRITE_SPEED = 13.3;
+        public const double DVD_READ_SPEED = 21.12;
+        public const double DVD_WRITE_SPEED = 21.12;
         public const double DVD_ONE_SIDE_STORAGE = 4812.8;
         public const double DVD_TWO_SIDE_STORAGE = 9216;
     }
